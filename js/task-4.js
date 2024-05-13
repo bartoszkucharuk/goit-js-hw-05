58d6e659cd672c2afd7681023c8bad8d526071da
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2024-05-13 18:09:51 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,7 +1,47 @@
-function getElementWidth(content, padding, border) {
-    const boxSizing = Number.parseFloat(content) + (Number.parseFloat(padding)*2) + (Number.parseFloat(border)*2);
-    return boxSizing;
-}
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+const getTotalBalanceByGender = (users, gender) =>
+    users
+        .filter(user => user.gender === gender)
+        .reduce((accumulator, allUsers) => accumulator + allUsers.balance, 0);
+
+
+const allUsers = [
+	{
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764
+  }
+];
+
+console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
+
+console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
